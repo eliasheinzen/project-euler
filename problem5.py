a2446ebcb225b5a61268646223503397409e241b
Elias Dacoregio Heinzen <eliasheinzen@gmail.com>
2018-09-30 12:55:08 -0300

    refactoring

--- problem5.py
+++ problem5.py
@@ -3,13 +3,24 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 # A: 232792560
 
+# Brute Force Solution ~> 5-6 secs
+# def is_divisible_by_integers_from_1_to_20(x):
+#     return x % 3 == 0 and x % 5 == 0 and x % 7 == 0 and x % 8 == 0 and x % 9 == 0 and x % 11 == 0 \
+#            and x % 12 == 0 and x % 13 == 0 and x % 14 == 0 and x % 15 == 0 and x % 16 == 0 and x % 17 == 0 \
+#            and x % 18 == 0 and x % 19 == 0
+#
+#
+# for i in range(20, 232792561, 20):
+#     if is_divisible_by_integers_from_1_to_20(i):
+#         print(i)
 
-def is_divisible_by_integers_until_20(x):
-    return x % 3 == 0 and x % 5 == 0 and x % 7 == 0 and x % 8 == 0 and x % 9 == 0 and x % 11 == 0 \
-           and x % 12 == 0 and x % 13 == 0 and x % 14 == 0 and x % 16 == 0 and x % 17 == 0 \
-           and x % 18 == 0 and x % 19 == 0
+primes, powers, a = [2, 3, 5, 7, 11, 13, 17, 19], [4, 2, 1, 1, 1, 1, 1, 1], [0 for i in range(8)]
+answer = 1
+print(primes, powers, a)
 
+for i in range(len(primes)):
+    a[i] = pow(primes[i], powers[i])
+    answer = answer * a[i]
+    print(primes[i], '^', powers[i], '=', a[i])
 
-for i in range(20, 232792561, 20):
-    if is_divisible_by_integers_until_20(i):
-        print(i)
+print('Resultado:', answer)
